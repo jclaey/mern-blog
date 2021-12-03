0112f444740310fc1d21fb743441f41e058015b8
Jason <jdclaeys@outlook.com>
2021-12-03 05:38:57 -0600

    create route and controller files

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import connectDB from './config/db.js';
+const dotenv = require('dotenv');
+const express = require('express');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
 connectDB();
 
+const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+app.use('/posts', postsRouter);
 
 const port = process.env.PORT || 5000;
 
