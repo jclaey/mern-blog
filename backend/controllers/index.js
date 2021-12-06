7f68409b3fda4aa81c1141f06b2eb87098b11b50
Jason <jdclaeys@outlook.com>
2021-12-06 02:50:13 -0600

    add authentication

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,11 +1,26 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
-// module.exports = {
-//   postLogin(req, res, next) {
-//     const { email, password } = req.body;
+module.exports = {
+  async postLogin(req, res, next) {
+    const { email, password } = req.body;
 
-//     const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
+    const matchPassword = async (inputPassword) => {
+      return await bcrypt.compare(inputPassword, user.password);
+    };
 
-//   }
-// };
+    if (user && (await matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: null
+      });
+    } else {
+      res.status(401);
+      throw new Error('Invalid email or password');
+    }
+  }
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-// const { asyncErrorHandler } = require('../middleware');
-// const {
-//   postLogin
-// } = require('../controllers/index');
+const { asyncErrorHandler } = require('../middleware');
+const {
+  postLogin
+} = require('../controllers/index');
 
-// router.post('/login', postLogin);
+router.post('/login', postLogin);
 
 module.exports = router;
