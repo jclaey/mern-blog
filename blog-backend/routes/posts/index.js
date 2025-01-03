6bafd6c445cd9ade8d730ff6f9d51c3f5e9eb3c5
Jason Claeys <jdclaeys@outlook.com>
2025-01-03 15:34:16 -0600

    add validators

--- blog-backend/routes/posts/index.js
+++ blog-backend/routes/posts/index.js
@@ -1,13 +1,29 @@
 import express from 'express'
 const router = express.Router()
+import multer from 'multer'
+import { storage } from '../../cloudinary/index.js'
+import { imageFileFilter } from '../../utils/fileFilter.js'
 import asyncHandler from '../../middleware/async.js'
+const upload = multer({ 
+    storage,
+    imageFileFilter,
+    limits: { fileSize: 1024 * 1024 * 5 }
+})
 
 import {
     getPosts, 
     postNew
 } from '../../controllers/posts/index.js'
 
-router.route('/').get(getPosts)
-router.route('/new').post(postNew)
+import { 
+    validatePostTitle,
+    validatePostContent
+} from '../validators.js'
+
+router.route('/').get(asyncHandler(getPosts))
+router.route('/new').post(upload.single('image'), [
+    validatePostContent,
+    validatePostTitle
+], asyncHandler(postNew))
 
 export default router
